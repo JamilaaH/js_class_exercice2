ab4a923101cbd64d815e55c66fd0af1541377f51
Jamila <jaja_mimi@hotmail.com>
2021-02-16 00:11:37 +0100

    debut exo

---
+++ public/js/main.js
@@ -0,0 +1,13 @@
+// exo 1 
+
+class Lieu {
+    constructor(nom, personne) {
+        this.nom = nom
+        this.personne = personne; 
+    }
+}
+
+let molenGeek = new Lieu("MolenGeek", [])
+let snack = new Lieu("snack", [])
+let maison = new Lieu("maison", [])
+
